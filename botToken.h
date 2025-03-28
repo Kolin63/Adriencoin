c9ea6e4fc0ab3075581302de605e53d677a51d4e
Kolin63 <theverylastgoodname@gmail.com>
2025-03-27 23:35:16 -0400

    Update botToken.h

--- botToken.h
+++ botToken.h
@@ -7,7 +7,7 @@
 
 inline std::string getBotToken()
 {
-    std::string filename{ "C:/discordtokens/adriencoin.txt" };
+    std::string filename{ "/root/token.txt" };
     std::ifstream fs{ filename, std::ios::binary };
 
     std::string token{};
