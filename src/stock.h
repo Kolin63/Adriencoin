dbb4ef89a0031a42cfce8fd976ac343dfa592a9b
Kolin <theverylastgoodname@gmail.com>
2025-03-30 21:16:12 -0400

    Start Making Stocks

---
+++ src/stock.h
@@ -0,0 +1,32 @@
+#ifndef STOCK_H
+#define STOCK_H
+
+#include <string>
+
+namespace adr
+{
+    class Stock
+    {
+    private:
+        const std::string& m_name{};
+        const Id m_id{};
+        // Value in Adriencoin
+        int m_value{};
+        // The time for all of the stocks, in days
+        static unsigned int time{};
+        
+    public:
+        enum Id {
+            jerrys_carrots,
+            divans_gems,
+            the_wood_and_fish_co,
+            aatroxs_slaying_co,
+            berts_knowledge,
+            the_brewers,
+            stonks_unlimited,
+            shens_investment,
+        };
+    };
+}
+
+#endif
