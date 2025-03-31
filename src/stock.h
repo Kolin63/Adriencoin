2e773464449fca3bc420d37ac53786b7c4070cc4
Kolin63 <theverylastgoodname@gmail.com>
2025-03-31 00:20:15 -0400

    Update stock.h

--- src/stock.h
+++ src/stock.h
@@ -2,6 +2,7 @@
 #define STOCK_H
 
 #include <string>
+#include <array>
 
 namespace adr
 {
@@ -17,6 +18,8 @@ namespace adr
             the_brewers,
             stonks_unlimited,
             shens_investment,
+
+            MAX
         };
 
     private:
@@ -24,16 +27,22 @@ namespace adr
         const Id m_id{};
         // Value in Adriencoin
         int m_value{};
+        
         // The time for all of the stocks, in days
         static unsigned int day;
+        // List of all stocks, gotten via getStock()
+        static std::array<Stock, MAX> stocks;
 
     public:
         static Stock& getStock(Id id); 
+
+        int getValue() const { return m_value; };
+        void setValue(int value) { m_value = value; };
+
         // Increments the day by 1
         static void newDay();
         static unsigned int getDay();
-
-
+        static void loadDay();
     };
 }
 
