8d0144049d8a5696935ddc38546dd2feb5fe617d
Kolin63 <theverylastgoodname@gmail.com>
2025-04-13 02:39:25 -0400

    Start Work on Graph

---
+++ graph.cpp
@@ -0,0 +1,61 @@
+#include <cassert>
+#include "graph.h"
+
+std::uint32_t adr::graph::coord_to_index(std::uint16_t x, std::uint16_t y) const
+{
+    assert(x * y < m_body.size() && "x * y must be less than body size");
+
+    // Adding 1 to the width is to account for any \n characters
+    int index{ y * (m_width + 1) + x };
+
+    assert(index < m_body.size() && "index must be less than body size");
+
+    return index;
+}
+
+void adr::graph::set_coord(std::uint16_t x, std::uint16_t y, char c)
+{
+    m_body[coord_to_index(x, y)] = c;
+}
+
+std::string adr::graph::make_body(std::uint8_t interval)
+{
+
+}
+
+std::uint32_t adr::graph::point_to_index(std::uint16_t x, std::uint16_t y) const
+{
+    const std::uint16_t gx{ static_cast<std::uint16_t>(x - ( (get_col_width() * x) + (get_row_num_width() + 1) )) };
+    const std::uint16_t gy{ static_cast<std::uint16_t>(get_height() - y) };
+
+    return coord_to_index(gx, gy);
+}
+
+void adr::graph::set_point(std::uint16_t x, std::uint16_t y, char c)
+{
+    m_body[point_to_index(x, y)] = c;
+}
+
+std::size_t adr::graph::get_col_width()
+{
+    m_col_width = get_col_width();
+    return get_col_width();
+}
+
+std::size_t adr::graph::get_col_width() const
+{
+    // Adding 1 is padding, so the numbers aren't directly next to eachother
+    return std::to_string(m_width).size() + 1;
+}
+
+std::size_t adr::graph::get_row_num_width()
+{
+    m_row_num_width = get_row_num_width();
+    return m_row_num_width;
+}
+
+std::size_t adr::graph::get_row_num_width() const
+{
+    // Does not account for the seperation pipe 
+    return std::to_string(m_height).size();
+}
